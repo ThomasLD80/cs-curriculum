4f5dd740bf1d589a680e49d271f886d6c24877e9
ThomasLD80 <144269397+ThomasLD80@users.noreply.github.com>
2023-10-30 11:41:03 -0700

    I am getting close to getting the turret to work.

--- Assets/Scripts/BlueFireball.cs
+++ Assets/Scripts/BlueFireball.cs
@@ -8,19 +8,29 @@ public class BlueFireball : MonoBehaviour
     public float moveSpeed;
 
     public float angleBetween;
-    public Transform target;
+    Rigidbody fireball_Rigidbody;
+
+    TurretFire turretScript;
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 5f;
+        Vector3 targetDir = turretScript.target.position - transform.position;
+        angleBetween = Vector3.Angle(transform.forward, targetDir);
+        fireball_Rigidbody = GetComponent<Rigidbody>();
+        
+        transform.Rotate(new Vector3(0f, 0f, angleBetween) * Time.deltaTime * moveSpeed, Space.World);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetDir = target.position - transform.position;
-        angleBetween = Vector3.Angle(transform.forward, targetDir);
-        transform.Rotate(0f, 0f, angleBetween);
-        transform.forward = new Vector3(moveSpeed,0f,0f);
+        //fireball_Rigidbody.AddForce(new Vector3(moveSpeed, 0f, 0f) * moveSpeed);
+        
+        fireball_Rigidbody.transform.position = new Vector3(
+            transform.position.x + (moveSpeed * Time.deltaTime),
+            transform.position.y,
+            transform.position.z
+            );
     }
 }
--- Assets/Scripts/TurretFire.cs
+++ Assets/Scripts/TurretFire.cs
@@ -6,23 +6,35 @@ using UnityEngine;
 public class TurretFire : MonoBehaviour
 {
     public GameObject fireball;
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target != null)
+        {
+            Instantiate(fireball, transform.position, transform.rotation);
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Instantiate(fireball, transform.position, transform.rotation);
+            target = other.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            target = null;
         }
     }
 }
