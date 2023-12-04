1e4c69a3e3908908fc1f7092810d250a720920a8
ThomasLD80 <144269397+ThomasLD80@users.noreply.github.com>
2023-12-04 10:46:33 -0800

    December 4th, 2023

--- Assets/Scripts/BlueFireball.cs
+++ Assets/Scripts/BlueFireball.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BlueFireball : MonoBehaviour
 {
     private GameObject player;
-    public float moveSpeed;
+    public float moveSpeed = 5f;
 
     public float angleBetween;
     Rigidbody fireball_Rigidbody;
@@ -14,12 +14,17 @@ public class BlueFireball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 5f;
+        
         Vector3 targetDir = turretScript.target.position - transform.position;
         angleBetween = Vector3.Angle(transform.forward, targetDir);
         fireball_Rigidbody = GetComponent<Rigidbody>();
         
         transform.Rotate(new Vector3(0f, 0f, angleBetween) * Time.deltaTime * moveSpeed, Space.World);
+
+        //Vector3 direction = turretScript.target.position - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x);
+        //Vector3 newLocation = transform.position + direction.normalized * Time.deltaTime;
+        //transform.rotation = Quaternion.Euler(0f, 0f, angle * Mathf.Rad2Deg);
     }
 
     // Update is called once per frame
@@ -27,10 +32,17 @@ public class BlueFireball : MonoBehaviour
     {
         //fireball_Rigidbody.AddForce(new Vector3(moveSpeed, 0f, 0f) * moveSpeed);
         
-        fireball_Rigidbody.transform.position = new Vector3(
-            transform.position.x + (moveSpeed * Time.deltaTime),
-            transform.position.y,
-            transform.position.z
-            );
+        //fireball_Rigidbody.transform.position = new Vector3(
+        //    transform.position.x + (moveSpeed * Time.deltaTime),
+        //    transform.position.y,
+        //    transform.position.z
+        //    );
+        
+        transform.Translate(new Vector3((moveSpeed * Time.deltaTime),
+            (moveSpeed * Time.deltaTime) - 2f,
+            0f
+            ));
+        
+        //fireball_Rigidbody.velocity = transform.forward * moveSpeed * Time.deltaTime;
     }
 }
