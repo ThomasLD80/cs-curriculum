a58dfc0958f4db568fc975a84c84584c5d1de42f
ThomasLD80 <144269397+ThomasLD80@users.noreply.github.com>
2024-01-30 11:40:43 -0800

    I made the jump script 2D.

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -5,38 +5,39 @@ using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
+    public bool onGround;
+
+    private float rayLength;
+
+    private Rigidbody2D m_Rigidbody;
+    private float jumpSpeed;
+
+    public LayerMask groundLayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rayLength = 1f;
+
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        jumpSpeed = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(transform.position, Vector2.down, Color.green);
         
-    }
+        onGround = Physics2D.Raycast(transform.position, Vector2.down, rayLength, groundLayer);
 
-    void FixedUpdate()
-    {
-        // Bit shift the index of the layer (22) to get a bit mask
-        int layerMask = 20 << 22;
-
-        // This would cast rays only against colliders in layer 22.
-        // But instead we want to collide against everything except layer 22. The ~ operator does this, it inverts a bitmask.
-        layerMask = ~layerMask;
-
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        if (onGround)
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
+            //Debug.Log("Hit the Ground");
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Debug.Log("jump");
+                m_Rigidbody.AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
+            }
         }
     }
 }
